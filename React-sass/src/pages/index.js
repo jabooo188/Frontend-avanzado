e2dcd1299c435d4ccee6e1ac7750a5f115ada4f8
jabooo188 <secretoadivina01@gmail.com>
2023-04-24 23:36:22 -0600

    Config de proyecto base

---
+++ React-sass/src/pages/index.js
@@ -0,0 +1,14 @@
+import Dashboard from "./Dashboard";
+import Home from "./Home";
+import login from "./login";
+import Secret from "./Secret";
+import Signup from "./Signup";
+
+export {
+    Dashboard,
+    login,
+    Home,
+    Secret,
+    Signup
+
+}
